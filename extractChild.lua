9aabce1c724cec3ab0640765e1e3f2c94ac8292d
Skeeve2 <skeevyskeeve@gmail.com>
2019-09-30 02:45:21 -0400

    Create extractChild.lua

---
+++ extractChild.lua
@@ -0,0 +1,13 @@
+function extractChild(element, children) -- Credit to dzikakulka. Extracts children/components easily. Modified by Skeeveo
+  for _, name in ipairs(children) do
+    if element.getChild(name) == nil then
+      log("Child does not exist: " .. name)
+      log("Avaliable children:")
+      for x, _ in pairs(element.getChildren()) do
+        log(x)
+      end
+    end
+    element = element.getChild(name)
+  end
+  return element
+end
